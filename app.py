7df93f25d9684352821a08c3fa4827374ae8c9b0
neibreen <azfaalharits25@gmail.com>
2024-12-26 21:28:51 +0700

    chore: add vue js

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ import pickle
 with open('model.pkl', 'rb') as f:
     rules = pickle.load(f)
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='./')
 
 @app.route('/')
 def index():
-    return render_template('index.html')  # Menyajikan halaman utama HTML
+    return render_template('index.html') 
 
 @app.route('/recommend', methods=['POST'])
 def recommend():
@@ -35,6 +35,10 @@ def recommend():
     except Exception as e:
         print(f"Error processing recommendation: {e}")
         return jsonify({'error': 'Internal Server Error'}), 500
+
+@app.route('/products', methods=['GET'])
+def products():
+    return jsonify(['Minuman', 'Burger', 'Sate', 'Bakso'])
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
